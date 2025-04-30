dddafa68571155dbf180cc7271198a4c2c9fd24c
Syed Muhammad Mudassir Naqvi <66870845+mudassirNaqvi@users.noreply.github.com>
2025-04-30 22:41:37 +0500

    Add files via upload

---
+++ firstapp.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+
+
+st.set_page_config(page_title="Start-Up Idea Generator", layout="wide")
+st.title(":blue[_Start-Up Idea_] _Generator_")
+st.header(":blue[_Welcome to Start-Up Idea Generator — your AI-powered innovation assistant, built in collaboration with the National Incubation Center_]" ,divider=True)
+st.markdown("***Have an idea? A problem to solve? Or maybe just a passion for innovation? This app helps you turn those sparks into full-fledged startup plans using advanced generative AI.***")
+
+multi = '''🔍 ***Describe a problem or theme***
+
+✨ ***Let our AI generate a startup plan for you***
+
+📈 ***Explore your idea’s potential in minutes***'''
+
+st.markdown(multi, unsafe_allow_html=True)
+st.page_link("pages/page_1.py", label=":blue[_Get Started_],",icon="➡️")
+
+placeholder_footer = st.empty()
+
+with placeholder_footer.container():
+    st.markdown("---")
+    st.markdown(
+        "<p style='text-align: center; font-size: 14px;'>"
+        "Created by <b>Syed Muhammad Mudassir Naqvi</b></p>",
+        unsafe_allow_html=True
+    )
---
+++ pages/page_1.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from google import genai
+
+st.set_page_config(page_title="Idea Generator", layout="wide")
+st.title(":bulb: Describe Your Idea or Problem")
+
+user_input = st.text_area("What problem are you trying to solve?", height=150)
+
+if st.button("Generate Startup Plan"):
+    if user_input.strip() == "":
+        st.warning("Please enter a problem or theme first.")
+    else:
+        st.success("Here's your AI-generated startup idea:")
+        st.write(f"**Problem:** {user_input}")
+
+        
+        client = genai.Client(api_key="AIzaSyAe8PzE8U2nNv9rEUq4d0XLGFTnkHeYMHY")
+
+        response = client.models.generate_content(
+        model="gemini-1.5-flash",
+        contents=f"""
+        You are an AI model strictly limited to generating startup ideas only.
+        If the user asks anything outside startup ideas, respond with:
+        'Sorry, I can only help you come up with startup ideas.'
+
+        Now respond to the following prompt accordingly:
+        {user_input}
+        """
+        )
+
+        st.markdown(f"**Solution:** {response.text}")
+        st.markdown("**Revenue Model:** Freemium + Subscription for expert access.")
+        st.markdown("**Target Audience:** Startups, students, professionals, and innovators.")
+
+placeholder_footer = st.empty()
+
+with placeholder_footer.container():
+    st.markdown("---")
+    st.markdown(
+        "<p style='text-align: center; font-size: 14px;'>"
+        "Created by <b>Syed Muhammad Mudassir Naqvi</b></p>",
+        unsafe_allow_html=True
+    )
